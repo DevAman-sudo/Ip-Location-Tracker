4fa9c56c4d6f35a9de47121fbeb82fd248c29bc6
DevAman-sudo <devaman.net@gmail.com>
2020-12-31 22:25:04 +0545

    Api Security Updates

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 // https://ipgeolocation.abstractapi.com/v1/?api_key=36f727e10cd5496baff661d9a434bbc9&ip_address=43.245.86.17
+async function apiKey() {
+	const response = await fetch('../ApiKey.json');
+	const jsonData = await response.json();
+	API_KEY = `${jsonData[0].API_KEY}` ;
+	return API_KEY ;
+}
+apiKey().then( response => {
+	const API_KEY = `${response}` ;
+})
+.catch( error => {
+	console.log(`Error Found => ${error}`);
+});
 
 const ip_address = `43.245.86.17` ;
 
 async function geoLocate(ip) {
-	const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=36f727e10cd5496baff661d9a434bbc9&ip_address=${ip}`);
-	console.log(api.json());
+	const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${API_KEY}&ip_address=${ip}`);
+	console.log(api);
 }
-geoLocate(ip_address);
+// geoLocate(ip_address);
