1cc67a4870fe4f8fd8dae68cb3295765f5e422de
DevAman-sudo <devaman.net@gmail.com>
2020-12-31 22:39:57 +0545

    Solved Bugs // Smooth Response Of Api

--- js/index.js
+++ js/index.js
@@ -1,21 +1,39 @@
-// https://ipgeolocation.abstractapi.com/v1/?api_key=36f727e10cd5496baff661d9a434bbc9&ip_address=43.245.86.17
-async function apiKey() {
-	const response = await fetch('../ApiKey.json');
-	const jsonData = await response.json();
-	API_KEY = `${jsonData[0].API_KEY}` ;
-	return API_KEY ;
-}
-apiKey().then( response => {
-	const API_KEY = `${response}` ;
-})
-.catch( error => {
-	console.log(`Error Found => ${error}`);
+//JvaScript DOM Elements //
+const form = document.getElementById('form');
+const textArea = document.getElementById('text-area');
+const button = document.getElementById('button');
+
+// Listening Event On Form Submit //
+form.addEventListener('submit', (event) => {
+	// Prevent Page To Submit //
+	event.preventDefault();
+
+	async function apiKey() {
+		const response = await fetch('../ApiKey.json');
+		const jsonData = await response.json();
+		API_KEY = `${jsonData[0].API_KEY}`;
+		return API_KEY;
+	}
+	apiKey().then(response => {
+
+			const API_KEY = `${response}`;
+			const ip_address = textArea.value;
+			// const ip_address = `43.245.86.17`;
+
+			async function geoLocate(ip) {
+				const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${API_KEY}&ip_address=${ip}`);
+				const jsonData = await api.json();
+				return jsonData;
+			}
+			geoLocate(ip_address).then(response => {
+					console.log(response);
+				})
+				.catch(error => {
+					console.log(`Error Found => ${error}`);
+				})
+
+		})
+		.catch(error => {
+			console.log(`Error Found => ${error}`);
+		});
 });
-
-const ip_address = `43.245.86.17` ;
-
-async function geoLocate(ip) {
-	const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${API_KEY}&ip_address=${ip}`);
-	console.log(api);
-}
-// geoLocate(ip_address);
