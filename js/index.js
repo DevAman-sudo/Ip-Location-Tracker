b3c5468c861825e750ca39e532b6033dd0769b9e
DevAman-sudo <devaman.net@gmail.com>
2021-01-03 19:38:00 +0545

    Search Functionality For Data Pull

--- js/index.js
+++ js/index.js
@@ -6,78 +6,65 @@ const dataContainer = document.getElementById('data-container');
 
 // function to append data //
 const appendData = (data) => {
-	let element = document.createElement('small');
-	element.innerText = data ;
-	dataContainer.appendChild(element);
+  let element = document.createElement('small');
+  element.innerText = data;
+  dataContainer.appendChild(element);
 }
 
 // fetching user public ip address //
-ipLocate()
-.then( response => {
-	let ip_address = response;
-	textArea.value = response;
-	appendData(`Your Public Ip Address Is ${response}`);
-	
-	apiKey().then( response => {
-		let API_KEY = response ;
-		
-		geoLocate(ip_address).then( response => {
-			console.log( response )
-			appendData( JSON.stringify(response) )
-		})
-	})
-	.catch( err => console.error(err) );
-})
-.catch( err => {
-	console.log(`User Public Ip Address Error => ${err}`);
-})
+const streamApiData = () => {
+
+  ipLocate()
+    .then(response => {
+      let ip_address = response;
+      textArea.value = response;
+      appendData(`Your Public Ip Address Is ${response}`);
+
+      apiKey().then(response => {
+          let API_KEY = response;
+
+          geoLocate(ip_address).then(response => {
+            console.log(response)
+            appendData(JSON.stringify(response))
+          })
+        })
+        .catch(err => console.error(err));
+    })
+    .catch(err => {
+      console.log(`User Public Ip Address Error => ${err}`);
+    })
+
+}
+streamApiData();
 
 // Listening Event On Form Submit //
 form.addEventListener('submit', (event) => {
-	// Prevent Page To Submit //
-	event.preventDefault();
+  // Prevent Page To Submit //
+  event.preventDefault();
 
-	// fetched response data from apiKey api //
-	apiKey().then(response => {
-			// Constant Variables //
-			let API_KEY = `${response}`;
-			let ip_address = textArea.value;
-
-			// fetched response data from geoLocate api //
-			geoLocate(ip_address).then(response => {
-					console.log(response);
-				})
-				// catch error while fetching data from ipgeolocation api //
-				.catch( err => {
-					console.log(`Error Found => ${err}`);
-				})
-		})
-		// catching error if key not fetched from ApiKey.json //
-		.catch( err => {
-			console.log(err);
-			console.log(`ipgeolocation Api Key Not Found`);
-		})
+  // fetched response data from apiKey api //
+  streamApiData();
 });
 
 // function to fetch ipgeolocation API_KEY //
 async function apiKey() {
-	const response = await fetch('../ApiKey.json');
-	const jsonData = await response.json();
-	API_KEY = `${jsonData[0].API_KEY}`;
-	return API_KEY;
+  const response = await fetch('../ApiKey.json');
+  const jsonData = await response.json();
+  API_KEY = `${jsonData[0].API_KEY}`;
+  return API_KEY;
 }
 
 // function to fetch user public ip ip_address //
 async function ipLocate() {
-	const api = await fetch('https://api.ipify.org?format=json');
-	const jsonData = await api.json();
-	const data = jsonData.ip;
-	return data;
+  const api = await fetch('https://api.ipify.org?format=json');
+  const jsonData = await api.json();
+  const data = jsonData.ip;
+  return data;
 }
 
 // function to fetch data of ip_address //
 async function geoLocate(ip) {
-	const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${API_KEY}&ip_address=${ip}`);
-	const jsonData = await api.text();
-	return jsonData;
+  const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${API_KEY}&ip_address=${ip}`);
+  const jsonData = await api.text();
+  return jsonData;
 }
