ca767accf3ba5020924894ca736356016778f9c4
DevAman-sudo <devaman.net@gmail.com>
2021-01-08 12:11:23 +0545

    fetched and stored user ip in database

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const nedb = require('nedb');
+const DataStore = require('nedb');
 const chalk = require('chalk');
 const path = require('path');
+const fetch = require('node-fetch');
+require('dotenv');
+
+const database = new DataStore({ filename: path.join( __dirname , 'database.db' ) });
+database.loadDatabase();
 
 const port = process.env.PORT || 8080 ;
 const staticPath = path.join( __dirname , '/public/');
 
 app.use(express.static(staticPath));
+
+async function userIp() {
+    let api = await fetch('https://api.ipify.org?format=json');
+    let jsonData = await api.json();
+    database.insert(jsonData);
+}
+userIp();
+
+// fetch('https://api.ipify.org?format=json').then( response => {
+    // console.log(response.json());
+// });
 
 app.get('/' , ( req , res ) => {
     res.sendFile( path.join( staticPath , 'index.html' ));
