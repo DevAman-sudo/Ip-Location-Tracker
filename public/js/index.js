d4cc830ff7c1ef96a5096abc5b37a469cdf3f05c
DevAman-sudo <devaman.net@gmail.com>
2021-01-09 13:02:03 +0545

    imported api_key by configed dotenv

--- public/js/index.js
+++ public/js/index.js
@@ -14,29 +14,7 @@ const appendData = (data) => {
   dataContainer.appendChild(element);
 };
 
-// function to fetch ipgeolocation API_KEY //
-async function apiKey() {
-  const response = await fetch('../ApiKey.json');
-  const jsonData = await response.json();
-  API_KEY = `${jsonData[0].API_KEY}`;
-  return API_KEY;
-}
-
-// function to fetch user public ip ip_address //
-async function ipLocate() {
-  const api = await fetch('https://api.ipify.org?format=json');
-  const jsonData = await api.json();
-  const data = jsonData.ip;
-  return data;
-}
-
-// function to fetch data of ip_address //
-async function geoLocate(ip) {
-  const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${API_KEY}&ip_address=${ip}`);
-  const jsonData = await api.text();
-  return jsonData;
-}
-
 socket.on('fetched-ip' , (userIp) => {
-    console.log(userIp);
+    textArea.value = userIp ;
+    appendData(userIp);
 });
--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const chalk = require('chalk');
 const path = require('path');
 const fetch = require('node-fetch');
 const WebSocket = require('socket.io');
-require('dotenv');
+require('dotenv').config();
 
-const database = new DataStore({ filename: path.join( __dirname , 'database.db' ) });
+const database = new DataStore({
+    filename: path.join(__dirname, 'database.db')
+});
 database.loadDatabase();
 
 const io = WebSocket(server);
-const port = process.env.PORT || 8080 ;
-const staticPath = path.join( __dirname , '/public/');
+const port = process.env.PORT || 8080;
+const GeoLocateKey = process.env.API_KEY;
+const staticPath = path.join(__dirname, '/public/');
 
 app.use(express.static(staticPath));
 
@@ -26,22 +29,34 @@ async function userIp() {
 }
 // userIp();
 
-app.get('/' , ( req , res ) => {
-    res.sendFile( path.join( staticPath , 'index.html' ));
+var A ;
+database.find(userIp, (err, data) => {
+    A = data[0].userIp.ip ;
+});
+console.log(A);
+
+// function to fetch data of ip_address //
+async function geoLocate(ip) {
+    const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${GeoLocateKey}&ip_address=${ip}`);
+    const jsonData = await api.text();
+    return jsonData;
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(staticPath, 'index.html'));
 });
 
 // web socket connection //
-io.on( 'connection' , (socket) => {
-    
-    database.find( userIp , (err , data) => {
-        let fetchedIp = data[0].userIp.ip ;
-        console.log(fetchedIp);
-        
-        socket.emit('fetched-ip' , fetchedIp);
+io.on('connection', (socket) => {
+
+    database.find(userIp, (err, data) => {
+        let fetchedIp = data[0].userIp.ip;
+
+        socket.emit('fetched-ip', fetchedIp);
     });
-    
+
 });
 
-server.listen( port , () => {
-    console.log( chalk.red.bgBlue.bold(`http://127.0.0.1:${port}`) );
+server.listen(port, () => {
+    console.log(chalk.red.bgBlue.bold(`http://127.0.0.1:${port}`));
 });
