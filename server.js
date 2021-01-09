96a2f9cbdfa47760cd2728801dba6aa62a97e15a
DevAman-sudo <devaman.net@gmail.com>
2021-01-09 18:17:32 +0545

    pushed geo locate api data to database

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const database = new DataStore({
 });
 database.loadDatabase();
 
+const databaseApi = new DataStore({
+    filename: path.join(__dirname, 'databaseApi.db')
+});
+databaseApi.loadDatabase();
+
 const io = WebSocket(server);
 const port = process.env.PORT || 8080;
 const GeoLocateKey = process.env.API_KEY;
@@ -29,17 +34,13 @@ async function userIp() {
 }
 // userIp();
 
-var A ;
-database.find(userIp, (err, data) => {
-    A = data[0].userIp.ip ;
-});
-console.log(A);
-
 // function to fetch data of ip_address //
 async function geoLocate(ip) {
     const api = await fetch(`https://ipgeolocation.abstractapi.com/v1/?api_key=${GeoLocateKey}&ip_address=${ip}`);
     const jsonData = await api.text();
-    return jsonData;
+    databaseApi.insert({
+        apiData: jsonData
+    });
 }
 
 app.get('/', (req, res) => {
@@ -51,7 +52,7 @@ io.on('connection', (socket) => {
 
     database.find(userIp, (err, data) => {
         let fetchedIp = data[0].userIp.ip;
-
+        geoLocate(fetchedIp);
         socket.emit('fetched-ip', fetchedIp);
     });
 
